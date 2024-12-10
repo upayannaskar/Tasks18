c2e7e16df67df08e8fcb7175958d245dc9dad2b2
upayannaskar5 <upayannaskar5@gmail.com>
2024-12-10 10:55:52 +0000

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.route.js"
+
+import {connectDB} from "./lib/db.js"
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(cookieParser())
+
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+    console.log("Server is running on http://localhost:" + PORT)
+    connectDB()
+})
